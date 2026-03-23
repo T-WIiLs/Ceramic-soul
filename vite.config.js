12e04fb17cb46d1b413413a62cb3997440044518
Сечкин Илья Игоревич <SechkinII@psm01.ru>
2026-03-23 14:51:40 +0300

    end

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+    base: "./",
     build: {
         rollupOptions: {
             input: {
